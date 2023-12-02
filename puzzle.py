fcff8ec1ec9bf93d3e3d0d532c27c688bd9ccfb7
Joanna Rykowska <joanna.rykowskaa@gmail.com>
2023-12-02 22:42:37 +0100

    lines of numbers

--- puzzle.py
+++ puzzle.py
@@ -9,13 +9,19 @@ def split_lines(l):
    return l.split('\n')
 
 lines = split_lines(puzzle)
-
+result = 0
 for line in lines:
     num = ""
+    n = 0
     for char in line:
         if char.isdigit():
             num = num + char
-    print(num)
+    #print(num)
+    n = num[0] + num[-1]
+    n_int = int(n)
+    #print(n_int)
+    result += + n_int
+print(result)
 
 
 
