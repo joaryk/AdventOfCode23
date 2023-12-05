aaa460778d8b57369739a212ebfc2224ab45cee3
Joanna Rykowska <joanna.rykowskaa@gmail.com>
2023-12-06 00:06:29 +0100

    Day 2

--- games.py
+++ games.py
@@ -14,26 +14,36 @@ rounds = ""
 cubes = ""
 cubes_map = {}
 max_map = {"red" : "12", "blue" : "14", "green" : "13"}
-is_possible = 0
+possible_games = []
 delimiters = [",", ";"]
+
+result = 0
 
 for line in games_txt:
     game_line = line.split(": ")
     games_map[game_line[0]] = game_line[1]
-for g in games_map:
-    #print(g, ":", games_map[g])
-    game = games_map[g]
+    game = game_line[1]
     rounds = game.split("; ")
+    all_rounds_ok = True
 
     for r in rounds:
         cubes = r.split(", ")
-    for c in cubes:
-        throw = c.split(" ")
-        cubes_map[throw[1]] = throw[0]
-    for x in cubes_map:
-        print(x, ":", cubes_map[x])
-
-    # for key in cubes_map:
-    #     if cubes_map[key] <= max_map[key]:
-    #         is_possible += 1
-#print(is_possible)
+        all_cubes_ok = True
+        for c in cubes:
+            throw = c.split(" ")
+            colour = throw[1]
+            value = int(throw[0])
+            max_value = int(max_map[colour])
+            #cubes_map[throw[1]] = throw[0]
+            if value > max_value:
+                all_cubes_ok = False
+                break
+        if all_cubes_ok == False:
+            all_rounds_ok = False
+            break
+    if all_rounds_ok == True:
+        possible_games.append(game_line[0])
+        #print(game_line[0])
+        num_of_game = int(game_line[0].split(" ")[1])
+        result += num_of_game
+print(result)
