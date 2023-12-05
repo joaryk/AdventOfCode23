55ed121623428e94570f3916cbb00a2555eec631
Joanna Rykowska <joanna.rykowskaa@gmail.com>
2023-12-05 17:30:08 +0100

    Day 2

--- games.py
+++ games.py
@@ -2,38 +2,38 @@ import re
 with open("games_input.txt", "r") as file:
     games_file = file.read()
 
-def create_map_of_games(cube, colour):
-    games_map = []
-    for i in range(len(cube)):
-        game = (cube[i], colour[i])
-        games_list.append(game)
-    return games_list
-
-def create_list_of_rounds(cube, colour):
-    games_list = []
-    for i in range(len(cube)):
-        games_dict = {cube[i] : colour[i]}
-        games_list.append(games_dict)
-    return games_list
-
 def split_lines(l):
-   return l.split('\n')
+    return l.split('\n')
+
 games_txt = split_lines(games_file)
 
-numbers = []
-colours = ["red","green","blue"]
-n = 0
-c = ''
+games_map = {}
+game_line = ""
+game = ""
+rounds = ""
+cubes = ""
+cubes_map = {}
+max_map = {"red" : "12", "blue" : "14", "green" : "13"}
+is_possible = 0
+delimiters = [",", ";"]
+
 for line in games_txt:
+    game_line = line.split(": ")
+    games_map[game_line[0]] = game_line[1]
+for g in games_map:
+    #print(g, ":", games_map[g])
+    game = games_map[g]
+    rounds = game.split("; ")
 
-    #list_of_games = create_list_of_games2(n,c)
+    for r in rounds:
+        cubes = r.split(", ")
+    for c in cubes:
+        throw = c.split(" ")
+        cubes_map[throw[1]] = throw[0]
+    for x in cubes_map:
+        print(x, ":", cubes_map[x])
 
-
-
-
-    # for char in line:
-    #     if char == ":":
-    #         key += char
-    #     elif:
-    #         break
-    #     value += char
+    # for key in cubes_map:
+    #     if cubes_map[key] <= max_map[key]:
+    #         is_possible += 1
+#print(is_possible)
