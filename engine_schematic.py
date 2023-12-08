6a7c138fb30ec250261dad117488efc786dca188
Joanna Rykowska <joanna.rykowskaa@gmail.com>
2023-12-08 15:43:44 +0100

    Day 3

---
+++ engine_schematic.py
@@ -0,0 +1,9 @@
+import re
+with open("engine_schematic.txt", "r") as file:
+    engine_file = file.read()
+
+def split_lines(l):
+    return l.split('\n')
+
+engine_file.txt = split_lines(engine_file)
+
