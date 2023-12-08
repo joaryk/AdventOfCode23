76a954f703f2cbe4c13c4be8d20aed17ce86076b
Joanna Rykowska <joanna.rykowskaa@gmail.com>
2023-12-08 15:26:29 +0100

    Day 2

--- games.py
+++ games.py
@@ -12,38 +12,38 @@ game_line = ""
 game = ""
 rounds = ""
 cubes = ""
-cubes_map = {}
-max_map = {"red" : "12", "blue" : "14", "green" : "13"}
-possible_games = []
+
 delimiters = [",", ";"]
 
 result = 0
+multiplication_results = []
 
 for line in games_txt:
+
+    highest_map = {"red": "0", "green": "0", "blue": "0"}
     game_line = line.split(": ")
     games_map[game_line[0]] = game_line[1]
     game = game_line[1]
     rounds = game.split("; ")
-    all_rounds_ok = True
+    multiplication = 1
 
     for r in rounds:
         cubes = r.split(", ")
-        all_cubes_ok = True
         for c in cubes:
             throw = c.split(" ")
             colour = throw[1]
             value = int(throw[0])
-            max_value = int(max_map[colour])
-            #cubes_map[throw[1]] = throw[0]
-            if value > max_value:
-                all_cubes_ok = False
-                break
-        if all_cubes_ok == False:
-            all_rounds_ok = False
-            break
-    if all_rounds_ok == True:
-        possible_games.append(game_line[0])
-        #print(game_line[0])
-        num_of_game = int(game_line[0].split(" ")[1])
-        result += num_of_game
-print(result)
+            highest_value = int(highest_map[colour])
+            if value > highest_value:
+                highest_map[colour] = value
+
+    for h in highest_map:
+        multiplication = multiplication * highest_map[h]
+        if h == 'blue':
+            multiplication_results.append(multiplication)
+
+# for f in range(len(multiplication_results)):
+#     print(multiplication_results[f])
+
+result = sum(multiplication_results)
+print("Wynik: ", result)
