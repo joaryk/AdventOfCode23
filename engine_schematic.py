238ebba9db550a64a83b4348dd46de9a4d28974a
Joanna Rykowska <joanna.rykowskaa@gmail.com>
2023-12-12 00:03:14 +0100

    Day 3

--- engine_schematic.py
+++ engine_schematic.py
@@ -20,27 +20,30 @@ num = 0
 sum = []
 is_dot = True
 result = 0
-# def checking_if_dot(engine_list, is_dot,inx_list, inx_char):
-#     if (engine_list[inx_list][inx_char-1]) != '.':
-#         is_dot = False
-#     elif (engine_list[inx_list][inx_char+1]) != '.':
-#         is_dot = False
-#     elif (inx_list > 0):
-#         if (engine_list[inx_list-1][inx_char]) != '.':
-#             is_dot = False
-#         elif (engine_list[inx_list-1][inx_char]) != '.':
-#             is_dot = False
-#         elif (engine_list[inx_list-1][inx_char+1]) != '.':
-#             is_dot = False
-#     elif (inx_list < 139):
-#         if (engine_list[inx_list+1][inx_char]) != '.':
-#             is_dot = False
-#         elif (engine_list[inx_list+1][inx_char]) != '.':
-#             is_dot = False
-#         elif (engine_list[inx_list+1][inx_char+1]) != '.':
-#             is_dot = False
-#     else:
-#         is_dot = True
+def checking_if_dot(engine_list,inx_list, inx_char):
+
+    if (inx_char != 0) and (engine_list[inx_list][inx_char-1]) != '.':
+        is_dot = False
+    # elif (inx_char < 139) and (engine_list[inx_list][inx_char+1]) != '.':
+    #     is_dot = False
+    elif (inx_list > 0):
+        if (inx_char != 0) and (engine_list[inx_list-1][inx_char-1]) != '.':
+            is_dot = False
+        elif (engine_list[inx_list-1][inx_char]) != '.':
+            is_dot = False
+        elif (inx_char < 139) and (engine_list[inx_list-1][inx_char+1]) != '.':
+            is_dot = False
+    elif (inx_list < 139):
+        if (engine_list[inx_list+1][inx_char]) != '.':
+            is_dot = False
+        elif (engine_list[inx_list+1][inx_char]) != '.':
+            is_dot = False
+        elif (inx_char < 139) and (engine_list[inx_list+1][inx_char+1]) != '.':
+            is_dot = False
+    else:
+        is_dot = True
+
+
 
 for line in engine_list:
     count_char = 0
@@ -51,7 +54,7 @@ for line in engine_list:
         if char.isdigit():
             is_digit = True
             is_number = True
-            # checking_if_dot(engine_list,is_dot, count_list, count_char)
+            checking_if_dot(engine_list, count_list, count_char)
 
         if (is_number == False):
             str_num = ''
@@ -59,12 +62,13 @@ for line in engine_list:
                 for n in num_list:
                     str_num = str_num + str(n)
                 num = int(str_num)
-                #if (is_dot == False):
-                sum.append(num)
-                num_list = []
-                num = 0
+                if (is_dot == False):
+                    sum.append(num)
+                    num_list = []
+                    num = 0
         if (is_digit == True):
             num_list.append(char)
+            index_of_digits = (count_list,count_char)
 
         count_char += 1
 count_list += 1
