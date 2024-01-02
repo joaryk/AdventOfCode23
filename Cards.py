35f20b0e5cfc9f22496a92922449ea16016101f3
Joanna Rykowska <joanna.rykowskaa@gmail.com>
2024-01-03 00:52:58 +0100

    Day 3

--- Cards.py
+++ Cards.py
@@ -3,13 +3,14 @@ import re
 cards_board = list(open('cards_schematic.txt'))
 
 winning_numbers = {}
-my_numbers = []
+my_numbers = {}
 
 for n, line in enumerate(cards_board):
-        x = re.split("[|]", line)[0]
-        winning_numbers.append(re.split("[:]", x)[1])
-        my_numbers.append(re.split("[|]", line)[1])
+        x = (re.split("[|]", line)[0]).strip()
+        winning_numbers[n] = (re.split("\s", (re.split("[:]", x)[1]).strip()))
 
-#zrobic split po spacji na obu listach (slownikach): x = re.split("\s", txt)
+        my_numbers[n] = (re.split("\s", (re.split("[|]", line)[1]).strip()))
+my_numbers = []
+
 #porownywac liczby z obu list (slownikow) findall albo inaczej x = re.findall("[4][5]", txt)
 
