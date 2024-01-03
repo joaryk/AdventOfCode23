fd93be9e1568b15102c5d34ac5e4d5fb111003a8
Joanna Rykowska <joanna.rykowskaa@gmail.com>
2024-01-03 14:32:08 +0100

    Day 3

--- Cards.py
+++ Cards.py
@@ -4,13 +4,19 @@ cards_board = list(open('cards_schematic.txt'))
 
 winning_numbers = {}
 my_numbers = {}
+winning_from_my_numbers = []
+winning_from_my_numbers_ = []
+results_in_card = []
 
 for n, line in enumerate(cards_board):
         x = (re.split("[|]", line)[0]).strip()
         winning_numbers[n] = (re.split("\s", (re.split("[:]", x)[1]).strip()))
-
         my_numbers[n] = (re.split("\s", (re.split("[|]", line)[1]).strip()))
-my_numbers = []
+
+for r in range(220):
+        winning_from_my_numbers = set(winning_numbers[r]) & set(my_numbers[r])
+        winning_from_my_numbers_ = [ x for x in winning_from_my_numbers if x.isdigit() ]
+        print(r, winning_from_my_numbers_)
 
 #porownywac liczby z obu list (slownikow) findall albo inaczej x = re.findall("[4][5]", txt)
 
