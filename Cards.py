f9072a8665fb57a5323ea20d8b28757411162c57
Joanna Rykowska <joanna.rykowskaa@gmail.com>
2024-01-03 14:45:48 +0100

    Day 3

--- Cards.py
+++ Cards.py
@@ -12,11 +12,21 @@ for n, line in enumerate(cards_board):
         x = (re.split("[|]", line)[0]).strip()
         winning_numbers[n] = (re.split("\s", (re.split("[:]", x)[1]).strip()))
         my_numbers[n] = (re.split("\s", (re.split("[|]", line)[1]).strip()))
+result = 0
 
 for r in range(220):
         winning_from_my_numbers = set(winning_numbers[r]) & set(my_numbers[r])
         winning_from_my_numbers_ = [ x for x in winning_from_my_numbers if x.isdigit() ]
-        print(r, winning_from_my_numbers_)
+        l = len(winning_from_my_numbers_)
+        if l == 0:
+                p = 0
+        elif l == 1:
+                p = 1
+        else:
+                p = 2 ** (l - 1)
+        print(r, l, p)
+        result += p
 
+print(result)
 #porownywac liczby z obu list (slownikow) findall albo inaczej x = re.findall("[4][5]", txt)
 
