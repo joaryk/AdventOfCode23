82652289792fe708218fbbce512629dee8a207cd
Joanna Rykowska <joanna.rykowskaa@gmail.com>
2024-01-04 15:21:01 +0100

    Day 4

--- Cards.py
+++ Cards.py
@@ -8,20 +8,23 @@ winning_from_my_numbers = []
 winning_from_my_numbers_ = []
 results_in_card = []
 
-counting_cards = {1 : 1}
+counting_cards = {}
+for i in range(220):
+        counting_cards[i] = 1
 
 for n, line in enumerate(cards_board):
         x = (re.split("[|]", line)[0]).strip()
         winning_numbers[n] = (re.split("\s", (re.split("[:]", x)[1]).strip()))
         my_numbers[n] = (re.split("\s", (re.split("[|]", line)[1]).strip()))
 
-
 for r in range(220):
         winning_from_my_numbers = set(winning_numbers[r]) & set(my_numbers[r])
         winning_from_my_numbers_ = [ x for x in winning_from_my_numbers if x.isdigit() ]
+        if r == 219:
+                break
         l = len(winning_from_my_numbers_)
-        for i in range(l):
-                counting_cards[r + (i + 1)] =+ 1
-        print(r, l)
-
+        for y in range(l):
+                counting_cards[r + (y + 1)] += 1
+print(counting_cards)
+ #zabezpieczyć końcówkę (gdy l jest większe niż y w counting_cards)
 
