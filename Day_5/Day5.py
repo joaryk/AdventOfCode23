4b2813b6af723c49dd6fbc5ce9d29264a9effa1b
Joanna Rykowska <joanna.rykowskaa@gmail.com>
2024-01-10 15:26:29 +0100

    Day 4

--- Day_5/Day5.py
+++ Day_5/Day5.py
@@ -24,13 +24,14 @@ seeds = [
     337630529]
 soils = []
 fertilizers = []
-water = []
+waters = []
 lights = []
 temperatures = []
 humidities = []
 locations = []
 
 for s in seeds:
+
     n = 0
     for x in Maps.SeedToSoil_:
         soil = int(Maps.SeedToSoil_lines[n][0])
@@ -41,4 +42,84 @@ for s in seeds:
             d = s - seed
             soil_sought = soil + d
             soils.append(soil_sought)
-            print(soil_sought)
+            #print(soil_sought)
+
+for o in soils:
+
+    n = 0
+    for x in Maps.SoilToFertilizer_:
+        fertilizer = int(Maps.SoilToFertilizer_lines[n][0])
+        soil = int(Maps.SoilToFertilizer_lines[n][1])
+        range_lenght = int(Maps.SoilToFertilizer_lines[n][2])
+        n += 1
+        if (o > soil) & (o < (o + range_lenght)):
+            d = o - soil
+            fertilizer_sought = fertilizer + d
+            fertilizers.append(fertilizer_sought)
+
+for f in fertilizers:
+
+    n = 0
+    for x in Maps.FertilizerToWater_:
+        water = int(Maps.FertilizerToWater_lines[n][0])
+        fertilizer = int(Maps.FertilizerToWater_lines[n][1])
+        range_lenght = int(Maps.FertilizerToWater_lines[n][2])
+        n += 1
+        if (f > fertilizer) & (f < (f + range_lenght)):
+            d = f - fertilizer
+            water_sought = water + d
+            waters.append(water_sought)
+
+for w in waters:
+
+    n = 0
+    for x in Maps.WaterToLight_:
+        light = int(Maps.WaterToLight_lines[n][0])
+        water = int(Maps.WaterToLight_lines[n][1])
+        range_lenght = int(Maps.WaterToLight_lines[n][2])
+        n += 1
+        if (w > water) & (w < (w + range_lenght)):
+            d = w - water
+            light_sought = light + d
+            lights.append(light_sought)
+
+for l in lights:
+
+    n = 0
+    for x in Maps.LightToTemperature_:
+        temperature = int(Maps.LightToTemperature_lines[n][0])
+        light = int(Maps.LightToTemperature_lines[n][1])
+        range_lenght = int(Maps.LightToTemperature_lines[n][2])
+        n += 1
+        if (l > light) & (l < (l + range_lenght)):
+            d = l - light
+            temperature_sought = temperature + d
+            temperatures.append(temperature_sought)
+
+for t in temperatures:
+
+    n = 0
+    for x in Maps.TemperatureToHumidity_:
+        humidity = int(Maps.TemperatureToHumidity_lines[n][0])
+        temperature = int(Maps.TemperatureToHumidity_lines[n][1])
+        range_lenght = int(Maps.TemperatureToHumidity_lines[n][2])
+        n += 1
+        if (t > temperature) & (t < (t + range_lenght)):
+            d = t - temperature
+            humidity_sought = humidity + d
+            humidities.append(humidity_sought)
+
+for h in humidities:
+
+    n = 0
+    for x in Maps.HumidityToLocation_:
+        location = int(Maps.TemperatureToHumidity_lines[n][0])
+        humidity = int(Maps.TemperatureToHumidity_lines[n][1])
+        range_lenght = int(Maps.TemperatureToHumidity_lines[n][2])
+        n += 1
+        if (h > humidity) & (h < (h + range_lenght)):
+            d = h - humidity
+            location_sought = location + d
+            locations.append(location_sought)
+
+min_loc = min(locations)
