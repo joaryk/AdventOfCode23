7a5d5b4e796c7ef07318665dc9360cdf9f1a5ee3
Joanna Rykowska <joanna.rykowskaa@gmail.com>
2024-01-18 23:44:58 +0100

    Day 4

--- Day_7/Day7_Part1.py
+++ Day_7/Day7_Part1.py
@@ -3,10 +3,29 @@ import re
 
 hands_txt = list(open('camel_cards.txt'))
 
-strenght_of_labels = {"2":1, "3":2, "4":3, "5":4, "6":5, "7":6, "8":7, "9":8, "T":9, "J":10, "Q":11, "K":12, "A":13}
+strenght_of_labels = {"2":"A", "3":"B", "4":"C", "5":"D", "6":"E", "7":"F", "8":"G", "9":"H", "T":"I", "J":"J", "Q":"K", "K":"L", "A":"M"}
 hands_labels_map = {}
 hands_strenght_map = {}
 hands_bid_map = {}
+
+def sorting_by_hands(hand_labels):
+
+    if len(hand_labels) == 1: #Five of a kind
+        return 7
+    elif len(hand_labels) == 2 and ("4" in hand_labels.values()) : #Four of a kind
+        return 6
+    elif len(hand_labels) == 2 and ("2" in hand_labels.values()) and ("3" in hand_labels.values()) : #Full house
+        return 5
+    elif len(hand_labels) == 3 and ("3" in hand_labels.values()) and ("1" in hand_labels.values()): #Three of a kind
+        return 4
+    elif len(hand_labels) == 3 and ("2" in hand_labels.values()) and ("1" in hand_labels.values()): #Two pair
+        return 3
+    elif len(hand_labels) ==4: # One pair
+        return 2
+    elif len(hand_labels) ==5: # High card
+        return 1
+def sorting_by_labels(strenght):
+
 
 
 for line in hands_txt:
@@ -16,13 +35,12 @@ for line in hands_txt:
 
 
 for n in hands_bid_map:
-    hand_labels = []
+    hand_labels = {}
     labels_in_hand = sorted(n)
     label_counter = {}
     g = itertools.groupby(labels_in_hand, lambda x : x)
     for label, group in g:
-        label_counter = {label: len(list(group))}
-        hand_labels.append(label_counter)
+        hand_labels[label] = len(list(group))
     hands_labels_map[n] = hand_labels
     print(n, label_counter)
 
