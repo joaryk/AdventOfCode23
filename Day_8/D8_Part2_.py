5a852dbdc654b62f95285fe01359cb454b37cd33
Joanna Rykowska <joanna.rykowskaa@gmail.com>
2024-02-15 17:14:31 +0100

    Part2

--- Day_8/D8_Part2_.py
+++ Day_8/D8_Part2_.py
@@ -1,12 +1,12 @@
 import re
+import math
 
 l_r = "LLRLRRLLRLRRLLRLRRLRRRLRLRLRRRLLRLRRRLRLRRRLRLRLLLRRLRLRLLRLRRLRRRLRRRLLRRLRLRRRLRRLRRRLRLLRRLRRRLRRRLRRLRLRRLLLRLRLLRRRLRRLLRLRLRRLLRLRRLLRLRRLRRLLRRRLRLRLRRRLLRRRLRRLRRRLRRRLRLRRRLRRLLLRRRLRLLLRRRLRLLRLLRRRLLRRLRRRLRRRLRLLRLRLRRRLLRRLRRRLRRLRLLRRRLRRLRRRLRRRLRRRLRLRRRLRRRLRLRRRR"
 l_r_instruction = [*l_r]
 
 map_txt = list(open('map_to_escape.txt'))
 dict_map = {}
-steps = 0
-not_finish = True
+all_steps = []
 start = []
 
 next_step = ""
@@ -22,22 +22,25 @@ for line in map_txt:
 start = [d for d in dict_map if d[-1] == 'A']
 
 
-while not_finish:
-    for n in l_r_instruction:
-        steps += 1
-        all = True
-        for count, s in enumerate(range(6)):
-            if n == "L":
+for count, s in enumerate(start):
+    loop = True
+    steps_to_Z = 0
+    steps_list = []
+    while loop:
+        for lr in l_r_instruction:
+            steps_to_Z += 1
+            if lr == "L":
                 start[count] = dict_map[s][0]
             else:
                 start[count] = dict_map[s][1]
-            all = all and s[-1] == 'Z'
-        # stop = [s for s in start if s[-1] == 'Z']
-        if all:
-            not_finish = False
-            break
+            if start[count][-1] == 'Z':
+                steps_list.append(steps_to_Z)
+            if dict_map[s][0] == dict_map[s][1]:
+                loop = False
+                all_steps.append(steps_list)
 
-print(steps)
-#
+                break
+print(math(all_steps))
+#metoda, która bierze kolejne start (z koncowka 'A') i zapisuje step w momencie 'Z' na koncu, konczy, kiedy trafia na ghosta
 
 
