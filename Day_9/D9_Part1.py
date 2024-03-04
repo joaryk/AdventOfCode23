6fddc0bc822143560e56335b52de108659730dcb
Joanna Rykowska <joanna.rykowskaa@gmail.com>
2024-03-04 22:51:53 +0100

    Part2

--- Day_9/D9_Part1.py
+++ Day_9/D9_Part1.py
@@ -5,9 +5,12 @@ oasis_txt = list(open('OASIS_report.txt'))
 def differences(line_of_oasis):
     list_of_differences = []
     list_of_values = line_of_oasis.split()
-    for value_ in list_of_values:
+
+    for count_, value_ in enumerate(list_of_values):
         v = int(value_)
-        print(v)
+        y = int(list_of_values[count_ - 1])
+        x = v - y
+        list_of_differences.append(x)
 
 for line in oasis_txt:
     differences(line)
